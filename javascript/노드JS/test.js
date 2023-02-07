44fb5ffa2c36278c303fc8059774678a5696ad1f
Taeseong <aka404365@gmail.com>
2023-02-07 15:09:57 +0900

    0207

--- javascript/노드JS/test.js
+++ javascript/노드JS/test.js
@@ -1,22 +1,33 @@
-var relationship1 = {
-  name: "zero",
-  friends: ["nero", "hero", "xero"],
-  logFriends: function () {
-    var that = this; // relationship1을 가리키는 this를 that에 저장
-    this.friends.forEach(function (friend) {
-      console.log(that.name, friend);
-    });
+var candyMachine = {
+  status: {
+    name: "node",
+    count: 5,
+  },
+  getCandy: function () {
+    this.status.count--;
+    return this.status.count;
   },
 };
-relationship1.logFriends();
 
-const relationship2 = {
-  name: "zero1",
-  friends: ["nero1", "zero1", "xero1"],
-  logFriends() {
-    this.friends.forEach((friend) => {
-      console.log(this.name, friend);
-    });
-  },
-};
-relationship2.logFriends();
+var getCandy = candyMachine.getCandy;
+var count = candyMachine.status.count;
+console.log(count);
+candyMachine.getCandy();
+console.log(count);
+// const candyMachine = {
+//   status: {
+//     name: "node",
+//     count: 5,
+//   },
+//   getCandy() {
+//     this.status.count--;
+//     return this.status.count;
+//   },
+// };
+// const {
+//   getCandy,
+//   status: { count },
+// } = candyMachine;
+
+// getCandy();
+// console.log(count);
